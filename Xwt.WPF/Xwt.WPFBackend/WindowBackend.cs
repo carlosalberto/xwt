eca3d68434f6fb171ee82f23fbbeda2856406425
Luis Reis <luiscubal@gmail.com>
2012-01-17 00:49:53 +0000

    Added missing copyright note.

--- Xwt.WPF/Xwt.WPFBackend/WindowBackend.cs
+++ Xwt.WPF/Xwt.WPFBackend/WindowBackend.cs
@@ -3,8 +3,10 @@
 //  
 // Author:
 //       Carlos Alberto Cortez <calberto.cortez@gmail.com>
+//       Luís Reis <luiscubal@gmail.com>
 // 
 // Copyright (c) 2011 Carlos Alberto Cortez
+// Copyright (c) 2012 Luís Reis
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
